0b32a72ea77f036d926e417add188d1a03255af4
Dan Trickey <git@trickey.io>
2023-03-22 17:47:28 +0000

    Significantly improve teams page

--- helpdesk/teams/urls.py
+++ helpdesk/teams/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from .views import TeamDetailView, TeamListView
+from .views import (
+    TeamDetailAboutView,
+    TeamDetailTicketsView,
+    TeamDetailTimelineView,
+    TeamListView,
+    TicketDetailRedirectView,
+)
 
 app_name = "teams"
 
 urlpatterns = [
     path("", TeamListView.as_view(), name="team_list"),
-    path("<slug:slug>", TeamDetailView.as_view(), name="team_detail"),
+    path("<slug:slug>/", TicketDetailRedirectView.as_view(), name="team_detail"),
+    path("<slug:slug>/about", TeamDetailAboutView.as_view(), name="team_detail_about"),
+    path("<slug:slug>/tickets", TeamDetailTicketsView.as_view(), name="team_detail_tickets"),
+    path("<slug:slug>/timeline", TeamDetailTimelineView.as_view(), name="team_detail_timeline"),
 ]
--- helpdesk/teams/views.py
+++ helpdesk/teams/views.py
@@ -1,23 +1,25 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DetailView
+from django.db.models import CharField, F, QuerySet, Value
+from django.views.generic import DetailView, RedirectView
 from django_filters.views import FilterView
 from django_tables2 import SingleTableMixin
 
 from tickets.filters import TeamTicketFilter
+from tickets.models import Ticket, TicketComment, TicketResolution
 from tickets.tables import TicketTable
 
 from .filters import TeamFilterset
 from .models import Team
 from .tables import TeamTable
 
-if TYPE_CHECKING:
-    from django.db.models import QuerySet
 
-    from tickets.models import Ticket
+class TicketDetailRedirectView(RedirectView):
+
+    pattern_name = "teams:team_detail_tickets"
 
 
 class TeamListView(LoginRequiredMixin, SingleTableMixin, FilterView):
@@ -25,11 +27,17 @@ class TeamListView(LoginRequiredMixin, SingleTableMixin, FilterView):
     table_class = TeamTable
     filterset_class = TeamFilterset
 
+class TeamDetailAboutView(LoginRequiredMixin, DetailView):
 
-class TeamDetailView(LoginRequiredMixin, SingleTableMixin, DetailView):
+    model = Team
+    slug_field = "tla"
+    template_name_suffix = "_detail_about"
+
+class TeamDetailTicketsView(LoginRequiredMixin, SingleTableMixin, DetailView):
     model = Team
     slug_field = "tla"
     table_class = TicketTable
+    template_name_suffix = "_detail_tickets"
 
     def get_ticket_queryset(self) -> QuerySet[Ticket]:
         return self.get_object().tickets.all()
@@ -52,3 +60,41 @@ class TeamDetailView(LoginRequiredMixin, SingleTableMixin, DetailView):
         context = super().get_context_data(**kwargs)
         context["filter"] = self.get_ticket_filter()
         return context
+
+
+class TeamDetailTimelineView(LoginRequiredMixin, DetailView):
+
+    model = Team
+    slug_field = "tla"
+    template_name_suffix = "_detail_timeline"
+
+    def get_entries(self) -> QuerySet[Any]:
+        fields = ("entry_type", "entry_timestamp", "entry_user", "entry_content", "entry_style_info")
+
+        tickets = Ticket.objects.filter(team=self.object).order_by().annotate(
+            entry_type=Value('Ticket Opened ', output_field=CharField()),
+            entry_timestamp=F("created_at"),
+            entry_user=F("opened_by"),
+            entry_content=F("description"),
+            entry_style_info=Value('is-info', output_field=CharField()),
+        ).values(*fields)
+
+        comments = TicketComment.objects.filter(ticket__team=self.object).order_by().annotate(
+            entry_type=Value('Ticket Comment', output_field=CharField()),
+            entry_timestamp=F("created_at"),
+            entry_user=F("author"),
+            entry_content=F("content"),
+            entry_style_info=Value('', output_field=CharField()),
+        ).values(*fields)
+
+        resolutions = TicketResolution.objects.filter(ticket__team=self.object).order_by().annotate(
+            entry_type=Value('Ticket Resolution', output_field=CharField()),
+            entry_timestamp=F("resolved_at"),
+            entry_user=F("user"),
+            entry_content=F("comment"),
+            entry_style_info=Value('is-success', output_field=CharField()),
+        ).values(*fields)
+        return comments.union(resolutions, tickets).order_by("-entry_timestamp")
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        return super().get_context_data(entries=self.get_entries(), **kwargs)
