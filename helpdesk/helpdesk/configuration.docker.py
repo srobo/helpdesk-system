a4c21680698e542aa9bfdb1bdb203376996fc83c
Dan Trickey <git@trickey.io>
2023-03-31 13:34:24 +0100

    Set timezone to Europe/London

--- helpdesk/helpdesk/configuration.docker.py
+++ helpdesk/helpdesk/configuration.docker.py
@@ -4,8 +4,6 @@ import dj_database_url
 
 ALLOWED_HOSTS = ["*"]
 
-# BASE_PATH = "helpdesk/"
-
 DATABASE = dj_database_url.config(
     conn_max_age=600,
     conn_health_checks=True,
@@ -18,6 +16,8 @@ DEBUG = False
 EMAIL = {
     "BACKEND": "django.core.mail.backends.dummy.EmailBackend",
 }
+
+TIME_ZONE = "Europe/London"
 
 SYSTEM_TITLE = "SR2023 Helpdesk"
 VOLUNTEER_SIGNUP_CODE = os.environ.get("VOLUNTEER_SIGNUP_CODE")
