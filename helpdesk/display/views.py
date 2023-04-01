acc5d0bcf428cfec4498a04b13e8f7749ccd3db5
Dan Trickey <git@trickey.io>
2023-04-01 19:11:47 +0100

    Formatting

--- helpdesk/display/views.py
+++ helpdesk/display/views.py
@@ -11,7 +11,9 @@ class HelpdeskDisplayView(TemplateView):
     template_name = "display/helpdesk.html"
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        qs = Ticket.objects.with_event_fields().exclude(Q(status=TicketStatus.RESOLVED) | Q(queue__show_in_overview=False))
+        qs = Ticket.objects.with_event_fields().exclude(
+            Q(status=TicketStatus.RESOLVED) | Q(queue__show_in_overview=False),
+        )
         in_progress = qs.filter(assignee_id__isnull=False)
         unassigned = qs.filter(assignee_id__isnull=True)
 
