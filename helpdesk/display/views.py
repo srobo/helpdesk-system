6da7f8f1f8b359f1c88a542bd595d14cf241aba1
Dan Trickey <git@trickey.io>
2023-04-01 19:11:12 +0100

    OR not AND

--- helpdesk/display/views.py
+++ helpdesk/display/views.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from django.db.models import Q
 from django.views.generic import TemplateView
 
 from tickets.models import Ticket, TicketStatus
@@ -10,7 +11,7 @@ class HelpdeskDisplayView(TemplateView):
     template_name = "display/helpdesk.html"
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        qs = Ticket.objects.with_event_fields().exclude(status=TicketStatus.RESOLVED, queue__show_in_overview=False)
+        qs = Ticket.objects.with_event_fields().exclude(Q(status=TicketStatus.RESOLVED) | Q(queue__show_in_overview=False))
         in_progress = qs.filter(assignee_id__isnull=False)
         unassigned = qs.filter(assignee_id__isnull=True)
 
