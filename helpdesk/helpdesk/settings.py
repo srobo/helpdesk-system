1cdaa8114ee84bf0fd6151b92eabff926a1fc038
Jake Howard <git@theorangeone.net>
2024-04-04 21:28:30 +0100

    Redirect to base path on logout

--- helpdesk/helpdesk/settings.py
+++ helpdesk/helpdesk/settings.py
@@ -193,7 +193,7 @@ STATIC_URL = f"/{BASE_PATH}static/"
 
 # Authentication URLs
 LOGIN_URL = f"/{BASE_PATH}auth/login/"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = BASE_PATH
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
