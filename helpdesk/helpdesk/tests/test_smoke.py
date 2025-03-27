7e25ae79a64efaf7481f8cb60a790cfcdd8cf017
Jake Howard <git@theorangeone.net>
2025-03-27 22:45:44 +0000

    Fix static navigation links
    You need to pass kwargs to model instances, otherwise weird things happen

--- helpdesk/helpdesk/tests/test_smoke.py
+++ helpdesk/helpdesk/tests/test_smoke.py
@@ -2,6 +2,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pytest
+from pytest_django.asserts import assertContains
+
+from core.models import NavigationLink
+from core.templatetags.navigation import STATIC_LINKS
+
 if TYPE_CHECKING:
     from django.test import Client
 
@@ -25,3 +31,12 @@ def test_onboarding(client: Client, admin_user: User) -> None:
     client.force_login(admin_user)
     resp = client.get("/accounts/onboarding/")
     assert resp.status_code == 200
+
+
+@pytest.mark.parametrize("link", STATIC_LINKS)
+def test_static_links(client: Client, admin_user: User, link: NavigationLink) -> None:
+    client.force_login(admin_user)
+    resp = client.get("/accounts/onboarding/")
+
+    assertContains(resp, link.url)
+    assertContains(resp, link.name)
